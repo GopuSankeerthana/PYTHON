9de161327da7400d11f2ce59d89af14a14ab8272
Sankeerthana Reddy <sankeerthanareddy@Sankeerthanas-MacBook-Air.local>
2026-06-17 22:10:12 +0530

    Added variables practice

---
+++ datatypes.py
@@ -0,0 +1,47 @@
+# Integer (Whole Numbers)
+age = 21
+print("Age:", age)
+print("Type:", type(age))
+
+
+# Float (Decimal Numbers)
+cgpa = 8.75
+print("CGPA:", cgpa)
+print("Type:", type(cgpa))
+
+# String (Text)
+name = "Sankeerthana"
+print("Name:", name)
+print("Type:", type(name))
+
+# Boolean (True/False)
+is_student = True
+print("Student:", is_student)
+print("Type:", type(is_student))
+
+# Taking User Input
+city = input("Enter your city: ")
+
+print("City:", city)
+print("Type:", type(city))
+
+# Type Conversion
+# String to Integer
+num = "100"
+num_int = int(num)
+
+print("Converted Integer:", num_int)
+print("Type:", type(num_int))
+
+# String to Float
+price = "99.99"
+price_float = float(price)
+
+print("Converted Float:", price_float)
+print("Type:", type(price_float))
+# Integer to String
+marks = 95
+marks_str = str(marks)
+
+print("Converted String:", marks_str)
+print("Type:", type(marks_str))
